0f69907e6cb3b25de84b84b980edc84d1ae01105
dsoluk <70490492+dsoluk@users.noreply.github.com>
2022-05-12 13:24:22 -0400

    work on signup

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ function openSignup(pricePlan) {
     window.open("./sign-up.html", "_blank")
 }
 
+// Validation / Error Handling on Form
+// --------------------------------------
+function checkEmpty (entered) {
+    if (entered !== "" && entered !== null) {
+        console.log("Is NOT empty")
+        return 
+    } else {
+        console.log("Value is empty so mark as red")
+        return `
+            <input type="text" id="name" name="name" placeholder=${entered}
+                class="clr-error border-err"><br><br>
+        `
+    }
+}
+
+//checkEmpty once submit button is clicked
+
+
+
+
+
+
+
+
+
+
+
+
+// const changeSelected = (e) => {
+//     const $select = document.querySelector('#mySelect');
+//     $select.value = 'free'
+//   };
+  
+//   document.querySelector('.changeSelected').addEventListener('click', changeSelected);
